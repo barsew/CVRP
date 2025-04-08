12d163785d89f33c80f65ca034583c5dbee96bb5
barsew <bartosz.seweryn@wp.pl>
2025-04-08 23:26:25 +0200

    init

--- src/algorithms/ACS.py
+++ src/algorithms/ACS.py
@@ -3,9 +3,11 @@ import random
 import os
 import json
 
+from torch.distributed.tensor import empty
+
 
 class AntColonyCVRP:
-    def __init__(self, graph, num_iterations=300, num_ants=10, alpha=1, beta=5, evaporation_rate=0.6, q=100, seed=0):
+    def __init__(self, graph, num_iterations=300, num_ants=10, alpha=1, beta=6, evaporation_rate=0.9, q=100, seed=0):
         """
         Algorytm mrówkowy dla CVRP.
 
@@ -28,17 +30,32 @@ class AntColonyCVRP:
         self.q = q
         self.pheromone = self.initialize_pheromones()
         self.seed = seed
+        self.distance_matrix = self.compute_distance_matrix()
+
+    def compute_distance_matrix(self):
+        """Precomputuje macierz odległości między wszystkimi wierzchołkami."""
+        nodes = list(self.graph.get_graph().nodes())
+        n = len(nodes)
+        dist_matrix = np.zeros((n, n))
+
+        for i in range(n):
+            for j in range(n):
+                if i != j:
+                    dist_matrix[i, j] = self.graph.euclidean_distance(self.graph.get_coordinates(nodes[i]), self.graph.get_coordinates(nodes[j]))
+        return dist_matrix
 
     def initialize_pheromones(self):
         """Inicjalizuje wartości feromonów na każdej krawędzi."""
         pheromone = {}
         for edge in self.graph.get_graph().edges():
-            pheromone[edge] = 1.0  # Początkowa ilość feromonów
+            v, u = edge
+            pheromone[(v, u)] = 1.0
+            pheromone[(u, v)] = 1.0
         return pheromone
 
     def heuristic(self, node1, node2):
         """Oblicza heurystykę jako odwrotność odległości."""
-        distance = self.graph.euclidean_distance(self.graph.get_coordinates(node1), self.graph.get_coordinates(node2))
+        distance = self.distance_matrix[node1 - 1, node2 - 1]
         return 1 / distance if distance > 0 else 1
 
     def select_next_node(self, current_node, unvisited, remaining_capacity, remaining_distance):
@@ -50,7 +67,7 @@ class AntColonyCVRP:
             demand = self.graph.get_demands()[node]
             distance = self.graph.euclidean_distance(self.graph.get_coordinates(current_node), self.graph.get_coordinates(node))
 
-            if demand <= remaining_capacity and distance <= remaining_distance:
+            if demand <= remaining_capacity and distance <= remaining_distance: # powinno sie patrzec czy na powort do depot starczy
                 tau = self.pheromone.get((current_node, node), 1.0) ** self.alpha
                 eta = self.heuristic(current_node, node) ** self.beta
                 probabilities.append(tau * eta)
@@ -62,41 +79,140 @@ class AntColonyCVRP:
         probabilities = np.array(probabilities)
         probabilities /= probabilities.sum()
 
-        np.random.seed(self.seed)
+        #np.random.seed(self.seed)
 
         return np.random.choice(nodes, p=probabilities)
+
+    def two_opt(self, route):
+        """
+        Wykonuje lokalną optymalizację trasy za pomocą heurystyki 2-opt.
+
+        :param route: Lista odwiedzanych wierzchołków (w tym powrotu do depotu)
+        :return: Optymalizowana trasa
+        """
+        subroutes = self.split_route_by_depot(route, 1)
+        for subroute in subroutes:
+            improved = True
+            while improved:
+                improved = False
+                for i in range(1, len(subroute) - 2):
+                    for j in range(i + 1, len(subroute) - 1):
+                        if j - i == 1:
+                            continue
+                        new_subroute = subroute[:i] + subroute[i:j + 1][::-1] + subroute[j + 1:]
+                        if self.calculate_route_cost(new_subroute) < self.calculate_route_cost(subroute):
+                            subroute = new_subroute
+                            improved = True
+        return self.join_subroutes_with_depots(subroutes, 1)
+
+    def split_route_by_depot(self, route, depot=1):
+        subroutes = []
+        current = []
+
+        for node in route:
+            if node == depot:
+                if current:
+                    subroutes.append(current)
+                    current = []
+            else:
+                current.append(node)
+
+        if current:
+            subroutes.append(current)
+
+        return subroutes
+
+    def join_subroutes_with_depots(self, subroutes, depot=1):
+        full_route = []
+        for subroute in subroutes:
+            full_route.append(depot)
+            full_route.extend(subroute)
+        full_route.append(depot)
+        return full_route
+
+    def calculate_route_cost(self, route):
+        """Oblicza całkowity koszt trasy."""
+        return sum(self.distance_matrix[route[i], route[i + 1]] for i in range(len(route) - 1))
+
+    def construct_solution_2_opt(self):
+        """Buduje rozwiązanie z wykorzystaniem mrówek i optymalizuje je heurystyką 2-opt."""
+        depot = self.graph.get_depot()
+        routes = [[] for _ in range(self.num_ants)]
+        total_cost = []
+
+        for ant in range(self.num_ants):
+            ant_route = [depot]
+            unvisited = set(self.graph.customers.keys())
+            remaining_capacity = self.graph.get_capacity()
+            remaining_distance = self.max_route_length
+            current_node = depot
+            current_visits_of_depot = 1
+
+            while unvisited and current_visits_of_depot <= self.num_vehicles:
+                next_node = self.select_next_node(current_node, unvisited, remaining_capacity, remaining_distance)
+                if next_node is None:
+                    current_visits_of_depot += 1
+                    remaining_capacity = self.graph.get_capacity()
+                    remaining_distance = self.max_route_length
+                    ant_route.append(depot)
+                    continue
+
+                ant_route.append(next_node)
+                remaining_capacity -= self.graph.get_demands()[next_node]
+                remaining_distance -= self.graph.euclidean_distance(self.graph.get_coordinates(current_node),
+                                                                    self.graph.get_coordinates(next_node))
+                unvisited.remove(next_node)
+                current_node = next_node
+
+            if current_visits_of_depot > self.num_vehicles and len(unvisited) != 0:
+                routes[ant] = []
+                total_cost.append(float('inf'))
+            else:
+                ant_route.append(depot)
+                optimized_route = self.two_opt(ant_route)
+                routes[ant] = optimized_route
+                total_cost.append(self.calculate_route_cost(optimized_route))
+
+        return routes, total_cost
 
     def construct_solution(self):
         """Buduje rozwiązanie z wykorzystaniem mrówek."""
         depot = self.graph.get_depot()
-        routes = [[] for _ in range(self.num_vehicles)]
-        total_cost = 0
-        unvisited = set(self.graph.customers.keys())
+        routes = [[] for _ in range(self.num_ants)]
+        total_cost = []
 
-        for vehicle in range(self.num_vehicles):
-            if not unvisited:
-                break
+        for ant in range(self.num_ants):
 
-            vehicle_route = [depot]
+            ant_route = [depot]
+            unvisited = set(self.graph.customers.keys())
             remaining_capacity = self.graph.get_capacity()
             remaining_distance = self.max_route_length
             current_node = depot
+            current_visits_of_depot = 1    # === current truck
 
-            while unvisited:
+            while unvisited and current_visits_of_depot <= self.num_vehicles:
                 next_node = self.select_next_node(current_node, unvisited, remaining_capacity, remaining_distance)
 
-                if next_node is None:  # Jeśli nie ma więcej możliwych ruchów, zakończ trasę
-                    break
+                if next_node is None:  # Jeśli nie ma więcej możliwych ruchów, ale sa nieodwiedzone to powrot do depotu i zmiana ciezarowki
+                    current_visits_of_depot += 1
+                    remaining_capacity = self.graph.get_capacity()
+                    remaining_distance = self.max_route_length
+                    ant_route.append(depot)
+                    continue
 
-                vehicle_route.append(next_node)
+                ant_route.append(next_node)
                 remaining_capacity -= self.graph.get_demands()[next_node]
                 remaining_distance -= self.graph.euclidean_distance(self.graph.get_coordinates(current_node), self.graph.get_coordinates(next_node))
                 unvisited.remove(next_node)
                 current_node = next_node
 
-            vehicle_route.append(depot)
-            routes[vehicle] = vehicle_route
-            total_cost += self.calculate_route_cost(vehicle_route)
+            if current_visits_of_depot > self.num_vehicles and len(unvisited) != 0:  # nie mamy trasy
+                routes[ant] = []
+                total_cost.append(float('inf'))
+            else:
+                ant_route.append(depot)
+                routes[ant] = ant_route
+                total_cost.append(self.calculate_route_cost(ant_route))
 
         return routes, total_cost
 
@@ -105,22 +221,47 @@ class AntColonyCVRP:
         for edge in self.pheromone:
             self.pheromone[edge] *= (1 - self.evaporation_rate)
 
+        # Aktualizacja feromonów na podstawie tras mrówek
+        for i, solution in enumerate(solutions):
+            if cost[i] == float('inf'):
+                continue  # Nie aktualizujemy, jeśli rozwiązanie jest złe
+
+            pheromone_deposit = self.q / cost[i]
+
+            for j in range(len(solution) - 1):
+                e = (solution[j], solution[j + 1])
+                reversed_e = (solution[j + 1], solution[j])
+
+                self.pheromone[e] = self.pheromone.get(e, 1.0) + pheromone_deposit
+                self.pheromone[reversed_e] = self.pheromone.get(reversed_e, 1.0) + pheromone_deposit
+
+    def update_pheromones_elite_ants(self, solutions, cost, best_solution, best_cost, elite_weight=5):
+        # Evaporacja feromonów
+        for edge in self.pheromone:
+            self.pheromone[edge] *= (1 - self.evaporation_rate)
+
         # Aktualizacja feromonów na podstawie rozwiązań mrówek
-        for solution in solutions:
-            pheromone_deposit = 1.0 / cost  # Możesz dostosować sposób depozytu
+        for i, solution in enumerate(solutions):
+            if cost[i] == float('inf'):
+                continue  # Ignorujemy złe rozwiązania
 
-            for i in range(len(solution) - 1):
-                edge = (solution[i], solution[i + 1])
-                reversed_edge = (solution[i + 1], solution[i])  # Obsługa grafu nieskierowanego
+            pheromone_deposit = self.q / cost[i]
 
-                if edge not in self.pheromone:
-                    self.pheromone[edge] = 0  # Inicjalizacja
+            for j in range(len(solution) - 1):
+                e = (solution[j], solution[j + 1])
+                reversed_e = (solution[j + 1], solution[j])
 
-                if reversed_edge not in self.pheromone:
-                    self.pheromone[reversed_edge] = 0  # Inicjalizacja
+                self.pheromone[e] = self.pheromone.get(e, 1.0) + pheromone_deposit
+                self.pheromone[reversed_e] = self.pheromone.get(reversed_e, 1.0) + pheromone_deposit
 
-                self.pheromone[edge] += pheromone_deposit
-                self.pheromone[reversed_edge] += pheromone_deposit  # Jeśli graf jest nieskierowany
+        elite_pheromone_deposit = elite_weight * (self.q / best_cost)
+
+        for j in range(len(best_solution) - 1):
+            e = (best_solution[j], best_solution[j + 1])
+            reversed_e = (best_solution[j + 1], best_solution[j])
+
+            self.pheromone[e] = self.pheromone.get(e, 1.0) + elite_pheromone_deposit
+            self.pheromone[reversed_e] = self.pheromone.get(reversed_e, 1.0) + elite_pheromone_deposit
 
     def calculate_route_cost(self, route):
         """Oblicza koszt trasy jako sumę długości krawędzi."""
@@ -128,13 +269,55 @@ class AntColonyCVRP:
 
     def solve(self):
         """Uruchamia algorytm mrówkowy."""
-        best_routes = None
+        best_rout = None
         best_cost = float('inf')
 
-        for _ in range(self.num_iterations):
+        for i in range(self.num_iterations):
             routes, cost = self.construct_solution()
-            if cost < best_cost:
-                best_routes, best_cost = routes, cost
-            self.update_pheromones(routes, cost)
+            #routes, cost = self.construct_solution_2_opt()
+            min_cost = min(cost)
+            idx = cost.index(min_cost)
 
-        return best_routes, best_cost
+            if min_cost < best_cost:
+                best_rout, best_cost = routes[idx], min(cost)
+            self.update_pheromones(routes, cost)
+            #self.update_pheromones_elite_ants(routes, cost, best_rout, best_cost)
+            # if i % 50 == 0:
+            #     print(f"Iteration {i}: Best Cost = {best_cost}")
+
+        return best_rout, best_cost
+
+    def solve_elite_ants(self):
+        """Uruchamia algorytm mrówkowy."""
+        best_rout = None
+        best_cost = float('inf')
+
+        for i in range(self.num_iterations):
+            routes, cost = self.construct_solution()
+            min_cost = min(cost)
+            idx = cost.index(min_cost)
+
+            if min_cost < best_cost:
+                best_rout, best_cost = routes[idx], min(cost)
+            self.update_pheromones_elite_ants(routes, cost, best_rout, best_cost)
+            # if i % 50 == 0:
+            #     print(f"Iteration {i}: Best Cost = {best_cost}")
+
+        return best_rout, best_cost
+
+    def solve_2_opt(self):
+        best_rout = None
+        best_cost = float('inf')
+
+        for i in range(self.num_iterations):
+            routes, cost = self.construct_solution_2_opt()
+            min_cost = min(cost)
+            idx = cost.index(min_cost)
+
+            if min_cost < best_cost:
+                best_rout, best_cost = routes[idx], min(cost)
+            self.update_pheromones(routes, cost)
+            # if i % 50 == 0:
+            #     print(f"Iteration {i}: Best Cost = {best_cost}")
+
+        return best_rout, best_cost
--- src/main.py
+++ src/main.py
@@ -1,37 +1,254 @@
-import argparse
-from utils.CVRPGraph import CVRPGraph, save_results
-from algorithms.greedy import CVRPNearestNeighbor
-from algorithms.ACS import AntColonyCVRP
-
-def main():
-    # parser = argparse.ArgumentParser(description="Load and process CVRP data")
-    # parser.add_argument("file", type=str, help="Path to the VRP file")
-    # args = parser.parse_args()
-
-    # Load the VRP data into the graph
-    cvrp_graph = CVRPGraph()
-    cvrp_graph.load_vrp_data("../data/A-n32-k5.vrp", 5, 300)
-
-    # Print some basic info
-    # print("Depot:", cvrp_graph.get_depot())
-    # print("Vehicle Capacity:", cvrp_graph.get_capacity())
-    # print("Demands:", cvrp_graph.get_demands())
-    # print("Graph Nodes:", cvrp_graph.get_graph().nodes(data=True))
-    # print("Graph Edges:", list(cvrp_graph.get_graph().edges(data=True)))
-
-    # Uruchomienie algorytmu zachłannego (Nearest Neighbor)
-    solver_greedy = CVRPNearestNeighbor(cvrp_graph)
-    routes_greedy, total_cost_greedy = solver_greedy.solve()
-    solver_greedy.display_routes()
-
-    # Algorytm mrówkowy
-    ant_solver = AntColonyCVRP(cvrp_graph)
-    routes_aco, total_cost_aco = ant_solver.solve()
-
-    # save_results(routes_greedy, total_cost_greedy, routes_aco, total_cost_aco)
-
-    print("Greedy solution:", routes_greedy, "Cost:", total_cost_greedy)
-    print("ACO solution:", routes_aco, "Cost:", total_cost_aco)
-
-if __name__ == "__main__":
-    main()
+# import argparse
+# from utils.CVRPGraph import CVRPGraph, save_results
+# from algorithms.greedy import CVRPNearestNeighbor
+# from algorithms.ACS import AntColonyCVRP
+# import time
+# import numpy as np
+# from collections import defaultdict
+# import matplotlib.pyplot as plt
+#
+#
+# def main():
+#     # parser = argparse.ArgumentParser(description="Load and process CVRP data")
+#     # parser.add_argument("file", type=str, help="Path to the VRP file")
+#     # args = parser.parse_args()
+#
+#     # Load the VRP data into the graph
+#     # files = ["A-n32-k5.vrp", "A-n34-k5.vrp", "A-n36-k5.vrp", "A-n44-k6.vrp", "A-n46-k7.vrp", "A-n48-k7.vrp",
+#     #         "A-n54-k7.vrp", "A-n55-k9.vrp", "A-n60-k9.vrp", "A-n62-k8.vrp", "A-n63-k10.vrp", "A-n64-k9.vrp"]
+#
+#     # files = ["A-n34-k5.vrp", "A-n44-k6.vrp", "A-n46-k7.vrp", "A-n48-k7.vrp",
+#     #           "A-n60-k9.vrp", "A-n62-k8.vrp", "A-n63-k10.vrp"]
+#     #
+#     # #optimal = [784, 778, 799, 937, 914, 1073, 1167, 1073, 1354, 1288, 1314, 1401]
+#     # optimal = [778, 937, 914, 1073, 1167, 1288, 1314]
+#     #
+#     cvrp_graph = CVRPGraph()
+#     cvrp_graph.load_vrp_data("../data/A-n63-k10.vrp", 10, 300)
+#     ant = AntColonyCVRP(cvrp_graph, num_iterations=300, num_ants=100)
+#     route, cost = ant.solve_2_opt()
+#     print("ACO solution:", route, "Cost:", cost)
+#     #
+#     # # graphs = []
+#     # #
+#     # # for file in files:
+#     # #     g = CVRPGraph()
+#     # #     k = file.find("k")
+#     # #     g.load_vrp_data("../data/" + file, file[k+1], 400)
+#     # #     graphs.append(g)
+#     #
+#     # results = defaultdict(dict)
+#     #
+#     # idx = 0
+#     #
+#     # for file in files:
+#     #     print(f"Przetwarzanie: {file}")
+#     #     k = int(file[file.find("k") + 1:file.find(".vrp")])
+#     #     graph = CVRPGraph()
+#     #     graph.load_vrp_data(f"../data/{file}", k, 400)
+#     #
+#     #     # --- 1. Greedy ---
+#     #     start = time.time()
+#     #     solver_greedy = CVRPNearestNeighbor(graph)
+#     #     greedy_route, greedy_cost = solver_greedy.solve()
+#     #     end = time.time()
+#     #     results[file]["Greedy"] = {"cost": greedy_cost, "time": end - start}
+#     #
+#     #     # --- 2. ACO ---
+#     #     ant = AntColonyCVRP(graph, num_iterations=300, num_ants=10)
+#     #     start = time.time()
+#     #     route, cost = ant.solve()
+#     #     end = time.time()
+#     #     results[file]["ACO"] = {"cost": cost, "time": end - start}
+#     #
+#     #     results[file]["Optimal"] = {"cost": optimal[idx], "time": 0}
+#     #     idx += 1
+#     #
+#     #     # --- 3. ACO + 2-opt ---
+#     #     ant2 = AntColonyCVRP(graph, num_iterations=300, num_ants=10)
+#     #     start = time.time()
+#     #     route2, cost2 = ant2.solve_2_opt()
+#     #     end = time.time()
+#     #     results[file]["ACO_2opt"] = {"cost": cost2, "time": end - start}
+#     #
+#     #     # --- 4. ACO  + Elite ---
+#     #     # ant3 = AntColonyCVRP(graph, num_iterations=300, num_ants=10)
+#     #     # start = time.time()
+#     #     # route3, cost3 = ant3.solve_elite_ants()
+#     #     # end = time.time()
+#     #     # results[file]["ACO_elite"] = {"cost": cost3, "time": end - start}
+#     #
+#     # methods = ["ACO", "ACO_2opt", "Optimal"]
+#     # colors = ["#90CAF9", "#66BB6A", "#FFCA28", "#EF5350"]
+#     # x = np.arange(len(files))
+#     # width = 0.2
+#     #
+#     # # WYKRES KOSZTÓW
+#     # plt.figure(figsize=(16, 6))
+#     # for i, method in enumerate(methods):
+#     #     cost_values = [results[file][method]["cost"] for file in files]
+#     #     plt.bar(x + i * width - width * 1.5, cost_values, width, label=method, color=colors[i])
+#     # plt.xticks(x, files, rotation=45, ha='right')
+#     # plt.ylabel("Całkowity koszt trasy")
+#     # plt.title("Porównanie jakości rozwiązania (koszt)")
+#     # plt.legend()
+#     # plt.tight_layout()
+#     # plt.grid(axis='y')
+#     # plt.savefig("koszty_algorytmy.png")
+#     # plt.show()
+#     #
+#     # # WYKRES CZASU
+#     # plt.figure(figsize=(16, 6))
+#     # for i, method in enumerate(methods):
+#     #     time_values = [results[file][method]["time"] for file in files]
+#     #     plt.bar(x + i * width - width * 1.5, time_values, width, label=method, color=colors[i])
+#     # plt.xticks(x, files, rotation=45, ha='right')
+#     # plt.ylabel("Czas działania [s]")
+#     # plt.title("Porównanie czasu działania")
+#     # plt.legend()
+#     # plt.tight_layout()
+#     # plt.grid(axis='y')
+#     # plt.savefig("czasy_algorytmy.png")
+#     # plt.show()
+#
+#     # Print some basic info
+#     # print("Depot:", cvrp_graph.get_depot())
+#     # print("Vehicle Capacity:", cvrp_graph.get_capacity())
+#     # print("Demands:", cvrp_graph.get_demands())
+#     # print("Graph Nodes:", cvrp_graph.get_graph().nodes(data=True))
+#     # print("Graph Edges:", list(cvrp_graph.get_graph().edges(data=True)))
+#
+#     results = {}
+#
+#     # Uruchomienie algorytmu zachłannego (Nearest Neighbor)
+#     # start = time.time()
+#     # solver_greedy = CVRPNearestNeighbor(cvrp_graph)
+#     # routes_greedy, total_cost_greedy = solver_greedy.solve()
+#     # end = time.time()
+#     # results['Greedy'] = {'cost': total_cost_greedy, 'time': end - start}
+#     #
+#     # # Algorytm mrówkowy
+#     # start = time.time()
+#     # ant_solver = AntColonyCVRP(cvrp_graph)
+#     # routes_aco, total_cost_aco = ant_solver.solve()
+#     # end = time.time()
+#     # results['ACO_basic'] = {'cost': total_cost_aco, 'time': end - start}
+#     #
+#     # start = time.time()
+#     # ant_solver = AntColonyCVRP(cvrp_graph)
+#     # routes_aco, total_cost_aco = ant_solver.solve_elite_ants()
+#     # end = time.time()
+#     # results['ACO_elite'] = {'cost': total_cost_aco, 'time': end - start}
+#     #
+#     # start = time.time()
+#     # ant_solver = AntColonyCVRP(cvrp_graph)
+#     # routes_aco, total_cost_aco = ant_solver.solve_2_opt()
+#     # end = time.time()
+#     # results['ACO_2opt'] = {'cost': total_cost_aco, 'time': end - start}
+#     #
+#     # # save_results(routes_greedy, total_cost_greedy, routes_aco, total_cost_aco)
+#     #
+#     # print("Greedy solution:", routes_greedy, "Cost:", total_cost_greedy)
+#     # print("ACO solution:", routes_aco, "Cost:", total_cost_aco)
+#     #
+#     # # Dane
+#     # labels = list(results.keys())
+#     # costs = [results[k]['cost'] for k in labels]
+#     # times = [results[k]['time'] for k in labels]
+#     #
+#     # # 1. Koszt (jakość rozwiązania)
+#     # plt.figure(figsize=(10, 5))
+#     # plt.bar(labels, costs, color='skyblue')
+#     # plt.title('Porównanie jakości (koszt całkowity)')
+#     # plt.ylabel('Całkowity koszt trasy')
+#     # plt.xlabel('Algorytm')
+#     # plt.grid(True, axis='y')
+#     # plt.tight_layout()
+#     # plt.savefig('../results/cost_comparison.png')
+#     # plt.show()
+#     #
+#     # # 2. Czas działania
+#     # plt.figure(figsize=(10, 5))
+#     # plt.bar(labels, times, color='salmon')
+#     # plt.title('Porównanie czasu działania')
+#     # plt.ylabel('Czas [s]')
+#     # plt.xlabel('Algorytm')
+#     # plt.grid(True, axis='y')
+#     # plt.tight_layout()
+#     # plt.savefig('../results/time_comparison.png')
+#     # plt.show()
+#     # Ładowanie danych
+#     # graph = CVRPGraph()
+#     # graph.load_vrp_data("../data/A-n32-k5.vrp", 5, 300)
+#     #
+#     # # Funkcja do testowania parametrów
+#     # def test_param_effect(param_name, values, fixed_params):
+#     #     results = []
+#     #
+#     #     for value in values:
+#     #         kwargs = fixed_params.copy()
+#     #         kwargs[param_name] = value
+#     #
+#     #         aco = AntColonyCVRP(graph, **kwargs)
+#     #         best_solution, cost = aco.solve()
+#     #         results.append(cost)
+#     #         print(f"{param_name}={value} => cost={cost}")
+#     #
+#     #     return results
+#     #
+#     # # Zakresy parametrów
+#     # alpha_vals = [0.5, 1, 1.5, 2, 3]
+#     # beta_vals = [2, 3, 4, 5, 6]
+#     # rho_vals = [0.3, 0.5, 0.6, 0.8, 0.9]
+#     # q_vals = [10, 50, 100, 200, 300]
+#     #
+#     # # Ustalone wartości domyślne
+#     # default_params = {
+#     #     "num_iterations": 100,
+#     #     "num_ants": 100,
+#     #     "alpha": 1,
+#     #     "beta": 5,
+#     #     "evaporation_rate": 0.6,
+#     #     "q": 100,
+#     #     "seed": 0
+#     # }
+#     #
+#     # # Testowanie i zbieranie wyników
+#     # alpha_results = test_param_effect("alpha", alpha_vals, default_params)
+#     # beta_results = test_param_effect("beta", beta_vals, default_params)
+#     # rho_results = test_param_effect("evaporation_rate", rho_vals, default_params)
+#     # q_results = test_param_effect("q", q_vals, default_params)
+#     #
+#     # # Wykresy
+#     # plt.figure(figsize=(12, 8))
+#     #
+#     # plt.subplot(2, 2, 1)
+#     # plt.plot(alpha_vals, alpha_results, marker='o')
+#     # plt.title("Wpływ α (feromonów)")
+#     # plt.xlabel("α")
+#     # plt.ylabel("Koszt trasy")
+#     #
+#     # plt.subplot(2, 2, 2)
+#     # plt.plot(beta_vals, beta_results, marker='o', color='green')
+#     # plt.title("Wpływ β (heurystyki)")
+#     # plt.xlabel("β")
+#     # plt.ylabel("Koszt trasy")
+#     #
+#     # plt.subplot(2, 2, 3)
+#     # plt.plot(rho_vals, rho_results, marker='o', color='orange')
+#     # plt.title("Wpływ współczynnika parowania (ρ)")
+#     # plt.xlabel("ρ")
+#     # plt.ylabel("Koszt trasy")
+#     #
+#     # plt.subplot(2, 2, 4)
+#     # plt.plot(q_vals, q_results, marker='o', color='red')
+#     # plt.title("Wpływ Q (feromon)")
+#     # plt.xlabel("Q")
+#     # plt.ylabel("Koszt trasy")
+#     #
+#     # plt.tight_layout()
+#     # plt.show()
+#
+#
+# if __name__ == "__main__":
+#     main()
